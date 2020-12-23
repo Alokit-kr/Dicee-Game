b4b558502e5a14a4d6b5ac314d2499ab0c4893b9
Alokit Kumar <64124927+Alokit-kr@users.noreply.github.com>
2020-12-23 08:08:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
  x.addEventListener("click",fun);
 function fun() {
   var randomNo1=Math.floor((Math.random())*6)+1;
-  var str1="images/dice"+randomNo1+".png";
+  var str1="dice"+randomNo1+".png";
   var position1=document.querySelector(".img1");
   position1.setAttribute("src",str1);
   // Player2
   var randomNo2=Math.floor((Math.random())*6)+1;
-  var str2="images/dice"+randomNo2+".png";
+  var str2="dice"+randomNo2+".png";
   var position2=document.querySelector(".img2");
   position2.setAttribute("src",str2);
   if(randomNo1==randomNo2)
